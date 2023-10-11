0db7b13539bb25fefc7c5d52fff6a9a206dd3bc8
Eric Kilmer <eric.d.kilmer@gmail.com>
2023-10-11 17:05:15 -0400

    Address suggestions

--- {{cookiecutter.project_slug}}/test/test_init.py
+++ {{cookiecutter.project_slug}}/test/test_init.py
@@ -1,13 +1,8 @@
 """Initial testing module."""
 import {{ cookiecutter.module_slug }}
 
-# ruff: noqa: S101 INP001
-# S101 Use of `assert` detected
-# INP001 implicit namespace package
-
 
 def test_version() -> None:
-    """Test whether the package version is valid."""
     version = getattr({{ cookiecutter.module_slug }}, "__version__", None)
     assert version is not None
     assert isinstance(version, str)
